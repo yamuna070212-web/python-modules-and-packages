1f63203437823a8b0e859d4a11f6b2973fa5cce8
Yamuna <yamuna070212@gmail.com>
2026-07-08 19:43:39 +0530

    Create Mini Calculator Using Modules calculator.py

---
+++ Mini Calculator Using Modules calculator.py
@@ -0,0 +1,23 @@
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+  main.py
+import calculator
+
+print(calculator.add(20,10))
+print(calculator.sub(20,10))
+print(calculator.mul(20,10))
+print(calculator.div(20,10))
+Output
+30
+10
+200
+2.0
